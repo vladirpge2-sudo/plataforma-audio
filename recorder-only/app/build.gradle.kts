aff51d7a432c2d60330f1e16763bf74d0d7b2ab7
vladirpge2-sudo <vladirpge2@gmail.com>
2026-08-05 11:58:37 -0300

    feat(recorder): configure Android application module

---
+++ recorder-only/app/build.gradle.kts
@@ -0,0 +1,37 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "br.com.vozemcamadas.recorder"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "br.com.vozemcamadas.recorder"
+        minSdk = 29
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
